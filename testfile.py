b02cc16a858a31f3a8c38ae946ec4b0f6a38c9e7
Ivan Korostylov <ivan.korostylyov@stryker.com>
2022-07-11 13:49:51 +0300

    update files.

--- testfile.py
+++ testfile.py
@@ -1,4 +1,31 @@
-from doctest import testfile
+#from doctest import testfile
+import pytest
 
+def multiply_by_two(x):
+  return x * 2
 
-testfile
+def test_multiply_by_two():
+  assert multiply_by_two(4) == 8
+
+def test_divide_by_zero():
+  with pytest.raises(ZeroDivisionError) as e:
+    num = 1 / 0
+  
+  assert 'division by zero' in str(e.value)
+
+# --------------------------------------------------------------------------------
+# A parametrized test function
+# --------------------------------------------------------------------------------
+
+products = [
+  (2, 3, 6),            # postive integers
+  (1, 99, 99),          # identity
+  (0, 99, 0),           # zero
+  (3, -4, -12),         # positive by negative
+  (-5, -5, 25),         # negative by negative
+  (2.5, 6.7, 16.75)     # floats
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+  assert a * b == product
